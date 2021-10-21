9e77a6fc6caa865e76c66ab3473974a5664b0143
Daniel <dvergara066@gmail.com>
2021-10-20 19:14:12 -0500

    remove previous files in the folder

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -4,7 +4,12 @@ DIR="/home/ubuntu/Deliah-resto-API"
 
 if [ -d "$DIR" ]; then
     echo "$DIR exists"
+    rm -rf ${DIR}/{*,.+}
 else
     echo "Creating ${DIR} directory"
     mkdir ${DIR}
 fi
+
+cd ${DIR}
+
+cp ../tmp/.env .env
